b0174a5d3d4bcf72fd69340833344fd7dda5b3d2
James Li <jamesli784@gmail.com>
2023-02-12 19:14:59 -0500

    side bar

--- scripts.js
+++ scripts.js
@@ -39,27 +39,77 @@ function zoomin(){
             mypin.style.width = (currWidth + 250) + "px";
         } 
     }
-    function zoomout(){
-        var myImg = document.getElementById("map1");
-        var currWidth = myImg.clientWidth;
-        if(currWidth == 1000) return false;
-		 else{
+function zoomout(){
+    var myImg = document.getElementById("map1");
+    var currWidth = myImg.clientWidth;
+    if(currWidth == 1000) return false;
+		else{
             myImg.style.width = (currWidth - 250) + "px";
             // myImg.style.height = auto;
         }
-        var mypin = document.getElementById("pin1");
-        var currWidth = mypin.clientWidth;
-        if(currWidth == 1000) return false;
-         else{
+    var mypin = document.getElementById("pin1");
+    var currWidth = mypin.clientWidth;
+    if(currWidth == 1000) return false;
+        else{
             mypin.style.width = (currWidth - 250) + "px";
         } 
     }
-    function openNav() {
+function openNav() {
   document.getElementById("mySidepanel").style.width = "32%";
 }
 
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "35%";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+function openNav1() {
+    document.getElementById("mySidebar1").style.width = "35%";
+}
+  
+function closeNav1() {
+    document.getElementById("mySidebar1").style.width = "0";
+}
+
+function openNav2() {
+    document.getElementById("mySidebar2").style.width = "35%";
+}
+  
+function closeNav2() {
+    document.getElementById("mySidebar2").style.width = "0";
+}
+
+function openNav3() {
+    document.getElementById("mySidebar3").style.width = "35%";
+}
+  
+function closeNav3() {
+    document.getElementById("mySidebar3").style.width = "0";
+}
+
+function openNav4() {
+    document.getElementById("mySidebar4").style.width = "35%";
+}
+  
+function closeNav4() {
+    document.getElementById("mySidebar4").style.width = "0";
+}
+
+// function openNav5() {
+//     document.getElementById("mySidebar5").style.width = "35%";
+// }
+  
+// function closeNav5() {
+//     document.getElementById("mySidebar5").style.width = "0";
+// }
+
+
 
 //scripts
